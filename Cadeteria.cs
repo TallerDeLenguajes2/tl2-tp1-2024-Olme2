491eadb10fae0c2055a7d7ac6fd0fc6d4cbbeb46
Valentino Olmedo <valenolme@gmail.com>
2024-08-29 14:05:08 -0300

    sexto commit (creo)

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -10,21 +10,57 @@ public class Cliente{
         telefono=Telefono;
         datosReferenciaDireccion=DatosReferenciaDireccion;
     }
+    public string? VerDireccionCliente(){
+        return direccion;
+    }
+    public string? VerNombreCliente(){
+        return nombre;
+    }
+    public string? VerTelefonoCliente(){
+        return telefono;
+    }
+    public string? VerRefCliente(){
+        return datosReferenciaDireccion;
+    }
 }
 public class Pedidos{
     private int numero{get;set;}
     private string? obs{get;set;}
     private Cliente? cliente{get;set;}
+    private string? estado{get;set;}
     public Pedidos(int Numero, string Obs, Cliente Cliente){
         numero=Numero;
         obs=Obs;
         cliente=Cliente;
+        estado="Sin entregar";
     }
-    public void verDireccionCliente(){
-
+    public void VerNombreCliente(){
+        Console.WriteLine("Nombre: "+cliente?.VerNombreCliente());
     }
-    public void verDatosCliente(){
-
+    public void VerDireccionCliente(){
+        Console.WriteLine("Direccion: "+cliente?.VerDireccionCliente());
+    }
+    public void VerTelefonoCliente(){
+        Console.WriteLine("Telefono: "+cliente?.VerTelefonoCliente());
+    }
+    public void VerRefCliente(){
+        Console.WriteLine("Datos de Referencia de Direccion: "+cliente?.VerRefCliente());
+    }
+    public void VerDatosCliente(){
+        VerNombreCliente();
+        VerDireccionCliente();
+        VerTelefonoCliente();
+        VerRefCliente();
+    }
+    public void EliminarCliente(){
+        if(cliente!=null){
+            cliente=null;
+        }else{
+            Console.WriteLine("El cliente no existe");
+        }
+    }
+    public void cambiarEstadoAEntregado(){
+        estado="Entregado";
     }
 }
 public class Cadete{
@@ -32,8 +68,9 @@ public class Cadete{
     private string? nombre{get;set;}
     private string? direccion{get;set;}
     private string? telefono{get;set;}
-    private List<Pedidos>? listadoPedidos{get;set;}
+    private List<Pedidos> listadoPedidos{get;set;}
     private double jornal{get;set;}
+    private int cantidadDePedidosEntregados{get;set;}
     public Cadete(int Id, string Nombre, string Direccion, string Telefono){
         id=Id;
         nombre=Nombre;
@@ -41,46 +78,88 @@ public class Cadete{
         telefono=Telefono;
         listadoPedidos=new List<Pedidos>();
         jornal=0;
+        cantidadDePedidosEntregados=0;
     }
-    public double jornalACobrar(){
-        double jornal;
-        jornal=0;
-        return jornal;
+    public void JornalACobrar(){
+        jornal=500*cantidadDePedidosEntregados;
     }
-    public void entregarPedido(Pedidos pedido){
-
+    public bool SeEncuentraPedido(Pedidos pedido){
+        return listadoPedidos.Contains(pedido);
     }
-    public void agregarPedido(Pedidos pedido){
-
+    public void EntregarPedido(Pedidos pedido){
+        if(SeEncuentraPedido(pedido)){
+            cantidadDePedidosEntregados++;
+            pedido.EliminarCliente();
+            pedido.cambiarEstadoAEntregado();
+            EliminarPedido(pedido);
+        }else{
+            Console.WriteLine("El pedido no se encuentra en la lista de pedidos");
+        }
     }
-    public void eliminarPedido(Pedidos pedido){
-         
+    public void AgregarPedido(Pedidos pedido){
+        if(SeEncuentraPedido(pedido)){
+            Console.WriteLine("El pedido ya se encuentra en la lista de pedidos");
+        }else{
+            listadoPedidos.Add(pedido);
+        }
     }
-    public List<Pedidos> listarPedidos(){
-        List<Pedidos> lista=new List<Pedidos>();
-        return lista;
+    public void EliminarPedido(Pedidos pedido){
+        if(SeEncuentraPedido(pedido)){
+           listadoPedidos.Remove(pedido);
+        }else{
+           Console.WriteLine("El pedido no se encuentra en la lista de pedidos");
+        }
     }
+    
 }
 public class Cadeteria{
     private string? nombre{get;set;}
     private string? telefono{get;set;}
-    private List<Cadete>? listadoCadetes{get;set;}
+    private List<Cadete> listadoCadetes{get;set;}
+    private int pedidosEntregados{get;set;}
+    private int pedidosAsignados{get;set;}
+    private int pedidosReasignados{get;set;}
+
     public Cadeteria(string Nombre, string Telefono){
         nombre=Nombre;
         telefono=Telefono;
         listadoCadetes=new List<Cadete>();
+        pedidosEntregados=0;
+        pedidosAsignados=0;
+        pedidosReasignados=0;
     }
-    public void asignarPedido(Cadete cadete, Pedidos pedido){
-
+    public void AsignarPedido(Cadete cadete, Pedidos pedido){
+        if(cadete.SeEncuentraPedido(pedido)){
+            Console.WriteLine("El pedido ya se encuentra asignado al cadete");
+        }else{
+            cadete.AgregarPedido(pedido);
+            pedidosAsignados++;
+        }
+    }
+    public void ReasignarPedido(Cadete viejo, Cadete nuevo, Pedidos pedido){
+        if(viejo==nuevo){
+            Console.WriteLine("El cadete no puede reasignar el pedido a si mismo");
+        }else if(nuevo.SeEncuentraPedido(pedido)){
+            Console.WriteLine("El pedido ya se encuentra asignado al nuevo cadete");
+        }else if(!viejo.SeEncuentraPedido(pedido)){
+            Console.WriteLine("El pedido no se encuentra asignado al viejo cadete, se lo asignara al nuevo");
+            nuevo.AgregarPedido(pedido);
+            pedidosAsignados++;
+        }else{
+            viejo.EliminarPedido(pedido);
+            nuevo.AgregarPedido(pedido);
+            Console.WriteLine("Pedido reasignado con exito");
+            pedidosReasignados++;
+        }
     }
     public string asignarInformeDeActividad(){
-        string? informe="a";
+        string? informe=$"Se asignaron {pedidosAsignados} pedidos\nSe entregaron {pedidosEntregados} pedidos\nSe reasignaron {pedidosReasignados}";
         return informe;
     }
     public void agregarCadete(Cadete cadete){
-
+        listadoCadetes.Add(cadete);
     }
     public void eliminarCadete(Cadete cadete){
-        
-    }
+        listadoCadetes.Remove(cadete);
+   }
 }
--- readme.md
+++ readme.md
@@ -7,14 +7,13 @@ Pedidos->Cadete Agregacion
 ¿Qué métodos considera que debería tener la clase Cadetería y la clase Cadete?
 Cadeteria:
     AsignarPedido(Cadete, Pedido)
-    GenerarInformeDeActividad()
+    AsignarInformeDeActividad()
     AgregarCadete(Cadete)
     EliminarCadete(Cadete)
 Cadete:
     EntregarPedido(Pedido)
     AgregarPedido(Pedido)
     EliminarPedido(Pedido)
-    ListarPedidos()
 
 Teniendo en cuenta los principios de abstracción y ocultamiento, ¿que atributos, propiedades y métodos deberían ser públicos y cuáles privados?.
 Todos los campos son privados y los metodos y propiedades son publicos.
@@ -50,4 +49,5 @@ public Cadeteria(string Nombre, string Telefono){
     }
 
 ¿Se le ocurre otra forma que podría haberse realizado el diseño de clases?
-Yo creo que el diseño esta bastante optimo, agregaria la variable double jornal a la clase cadete.
+Yo creo que el diseño esta bastante optimo, agregaria la variable double jornal a la clase cadete. Luego, para tener los atributos private y que puedan ser usados por otras clases, agregaria
+metodos que devuelven dichos atributos cuando son invocados desde otras clases para seguir el concepto de encapsulacion.
